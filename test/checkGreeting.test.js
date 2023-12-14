548fc7eae8cfe638c38d7d0190765e617c294a8c
Robert <robert.ailenei@student.vinci.be>
2023-12-14 18:03:32 +0100

    deleted babel from dep moved in dev dep

--- test/checkGreeting.test.js
+++ test/checkGreeting.test.js
@@ -8,23 +8,6 @@ test('n°1: greet function to return "Hello, Jean-Kevin." if name is "Jean-Kevin
 
   expect(result).toBe('Hello, Jean-Kevin.')
 })
-
-test('n°2.1: greet function to return "Hello, my friend." if name is null', () => {
-  const name = null
-
-  const result = greet(name)
-
-  expect(result).toBe('Hello, my friend.')
-})
-
-test('n°2.2: greet function to return "Hello, my friend." if name is undefined', () => {
-  const name = undefined
-
-  const result = greet(name)
-
-  expect(result).toBe('Hello, my friend.')
-})
-
 test('n°2.3: greet function to return "Hello, my friend." if name is an empty String', () => {
   const name = ''
 
